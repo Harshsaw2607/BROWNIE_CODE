349ecc82e9b909f3b1721ebd1144d8cbf5595440
Harshsaw2607 <110737669+Harshsaw2607@users.noreply.github.com>
2022-10-14 13:11:25 +0530

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,14 @@
+# from brownie import*
+
+from brownie import accounts
+def deploy_simple_storage():
+    account=accounts[0]
+    print(account)
+    pass
+
+    def main():
+        deploy_simple_storage()
+        
+
+        if _name_=="_main_":
+            main()
